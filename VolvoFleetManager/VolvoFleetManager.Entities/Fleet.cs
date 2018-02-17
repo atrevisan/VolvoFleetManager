09833710db0fe44bb9a8888c0876ba92bae76c4b
atrevisan <allan.caminha.trevisan@gmail.com>
2018-02-17 10:01:06 -0200

    Added Entities and data projects

---
+++ VolvoFleetManager/VolvoFleetManager.Entities/Fleet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolvoFleetManager.Entities
+{
+    public class Fleet
+    {
+        public IEnumerable<Vehicle> Vehicles { get; set; }
+    }
+}
---
+++ VolvoFleetManager/VolvoFleetManager.Entities/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VolvoFleetManager.Entities.Enumerations;
+
+namespace VolvoFleetManager.Entities
+{
+    public class Vehicle
+    {
+        public string ChassisId { get; set; }
+
+        public VehicleType Type { get; set; }
+
+        public byte NumberOfPassemgers
+        {
+            get
+            {
+                switch (Type)
+                {
+                    case VehicleType.Bus:
+                        return 42;
+                    case VehicleType.Truck:
+                        return 1;
+                    case VehicleType.Car:
+                        return 4;
+                    default:
+                        return 0;
+                }
+            }
+        }
+
+        public string Color { get; set; }
+
+    }
+}
